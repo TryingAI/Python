27c988e275d07998fb9294c80255b22b53894827
TryingAI <sganguly03@gmail.com>
2019-06-16 02:41:01 +0530

    Python Lists

--- ListComprehension.py
+++ ListComprehension.py
@@ -1,7 +1,6 @@
 """
 Hackerrank List Comprehension Problem
 Created on 30/05/2019
-@author: SjGanguly (trying_ai)
 
 Let's learn about list comprehensions! You are given three integers X,Y and Z representing the dimensions of a cuboid along with an integer N.
 You have to print a list of all possible coordinates given by (i,j,k) on a 3D grid where the sum of i+j+k is not equal to N .
@@ -14,6 +13,7 @@ Sample Input
 
 Sample Output
 [[0, 0, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0], [1, 1, 1]]
+Solution: @author: SjGanguly (trying_ai)
 """
 if __name__ == '__main__':
     x = int(input('x: '))
